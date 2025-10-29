f715c4cd833631351673fdc8dbf85d5a2e39a5ac
catitw <catitw@outlook.com>
2025-10-29 11:21:03 +0800

    update(neovide): add copy paste key mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,14 @@ local map = vim.keymap.set
 -- - https://www.reddit.com/r/AstroNvim/comments/1hkb6pr/remap_toggle_comment_to_ctrl
 -- - https://github.com/LazyVim/LazyVim/discussions/3033
 map({ 'n', 'v' }, '<leader>/', 'gcc', { remap = true, desc = 'Toggle comment' })
+
+if vim.g.neovide then
+  map('v', '<C-S-c>', '"+y') -- Copy in visual mode
+  map('v', '<C-S-v>', '"+P') -- Paste in visual mode
+
+  map('n', '<C-S-v>', '"+P') -- Paste in normal mode
+
+  map('c', '<C-S-v>', '<C-R>+') -- Paste in command mode
+
+  map('i', '<C-S-v>', '<C-R>+') -- Paste in insert mode
+end
