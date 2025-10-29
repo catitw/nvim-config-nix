4343e084cebbcc29c880e53e51e1cb66399e0d6c
catitw <catitw@outlook.com>
2025-10-29 09:46:02 +0800

    feat(neovide): add `neovide` config

--- init.lua
+++ init.lua
@@ -18,6 +18,23 @@ local lazyOptions = {
   lockfile = getlockfilepath(),
 }
 
+if vim.g.neovide then
+  vim.g = {
+    -- configure scaling
+    neovide_scale_factor = 1.0,
+    -- configure padding
+    neovide_padding_top = 0,
+    neovide_padding_bottom = 0,
+    neovide_padding_right = 0,
+    neovide_padding_left = 0,
+
+    neovide_refresh_rate = 240,
+    neovide_cursor_antialiasing = true,
+    neovide_cursor_vfx_mode = 'railgun',
+    neovide_transparency = 0.95,
+  }
+end
+
 -- NOTE: this the lazy wrapper. Use it like require('lazy').setup() but with an extra
 -- argument, the path to lazy.nvim as downloaded by nix, or nil, before the normal arguments.
 require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 'lazy.nvim' }, {
